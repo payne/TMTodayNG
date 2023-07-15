2381ebeca35c519851146bf10f5cfd78da331a54
Matt Payne <Payne@MattPayne.org>
2023-07-14 20:59:07 -0500

    Added about note and a link to the github repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {SpeakersComponent} from "./speakers/speakers.component";
 import {MeetingDatesComponent} from "./meeting-dates/meeting-dates.component";
+import {AboutComponent} from "./about/about.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'speakers', component: SpeakersComponent},
   { path: 'meeting_dates', component: MeetingDatesComponent},
+  { path: 'about', component: AboutComponent},
   { path: '**', redirectTo: '' }
 ];
 
