3a69d147101c155d1ce013773f6565d1f07c651b
Matt Payne <Payne@MattPayne.org>
2023-07-14 20:41:56 -0500

    ng generate component speakers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimpleMenu1Component } from './simple-menu1/simple-menu1.component';
 import { HomeComponent } from './home/home.component';
+import { SpeakersComponent } from './speakers/speakers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleMenu1Component,
-    HomeComponent
+    HomeComponent,
+    SpeakersComponent
   ],
   imports: [
     BrowserModule,
