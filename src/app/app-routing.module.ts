f57156be90e7fb9de2a4716fb32433d53fd42a9b
Matt Payne <Payne@MattPayne.org>
2023-07-14 20:48:00 -0500

    Added routes and simple text placeholders.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {SpeakersComponent} from "./speakers/speakers.component";
+import {MeetingDatesComponent} from "./meeting-dates/meeting-dates.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'speakers', component: SpeakersComponent},
+  { path: 'meeting_dates', component: MeetingDatesComponent},
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
