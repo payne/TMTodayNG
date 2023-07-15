4f2396ffcadc0205286b9be62032373ff0db0e00
Matt Payne <Payne@MattPayne.org>
2023-07-14 20:41:19 -0500

    ng generate component home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimpleMenu1Component } from './simple-menu1/simple-menu1.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleMenu1Component
+    SimpleMenu1Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
